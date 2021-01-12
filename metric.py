cd788665d60b8c42a47f435fad64faaf112a81d3
furkanbiten <furkanbiten@gmail.com>
2021-01-12 14:01:48 +0100

    made it more usable outside of metric.py

--- metric.py
+++ metric.py
@@ -8,39 +8,38 @@ import pandas as pd
 
 from collections import defaultdict
 
+
 class Metric:
-    def __init__(self, args):
-        self.args = args
-        self.IMG_THRESHOLD = int(args.threshold)
+    def __init__(self, metric, gt, sims, recall_type, score, metric_name, recall_thresholds=[1,5,10], threshold=1,
+                 dataset='coco', include_anns=False, model_name='None'):
+
+        assert(type(score) is list)
+        assert(type(recall_thresholds) is list)
+
+        self.IMG_THRESHOLD = int(threshold)
         self.FUNCTION_MAP = {'hard': self.hard, 'soft': self.soft, 'softer': self.softer}
         self.TEXT_PER_IMG = 5
-        self.RECALL_THRESHOLDS = args.recall_thresholds
+        self.RECALL_THRESHOLDS = recall_thresholds
         self.TOP_K = self.RECALL_THRESHOLDS[-1]
 
-        if args.dataset == 'coco':
-            with open(os.path.join(args.dataset_path, args.dataset + '_test.json')) as fp:
-                self.gt = json.load(fp)
-        else:
-            with open(os.path.join(args.dataset_path, args.dataset + '_dataset.json')) as fp:
-                self.gt = json.load(fp)
+        self.metric_name = metric_name
+        self.metric = metric
+        self.gt = gt
+        self.sims = sims
+        self.dataset = dataset
+        self.include_anns = include_anns
+        self.recall_type = recall_type
+        self.score = score
+        self.model_name = model_name
 
-        if args.metric == 'spice':
-            metric = pd.read_csv(os.path.join(args.metric_path, args.dataset + '_' + args.metric + '.csv'), sep=',',
-                                 header=None)
-            metric = metric.to_numpy()
-            if args.dataset == 'coco': self.metric = metric[:, :5000]
-            if args.dataset == 'f30k': self.metric = metric[:, :1000]
-
-        elif args.metric == 'cider':
-            self.metric = np.load(os.path.join(args.metric_path, args.dataset+'_cider.npy'))
-
-        filename = os.path.join(args.metric_path, 'sims_' + args.model_name + '_' + args.dataset + '_precomp.json')
-        self.sims = json.load(open(filename, 'r'))
-
+        self.intersection = []
         self.get_intersection()
 
+    def set_sims(self, sims):
+        self.sims = sims
+
     def get_intersection(self):
-        self.intersection = []
+
         for ix in tqdm.tqdm(range(len(self.sims)), leave=False):
             # GET MOST RELEVANT NON-GROUND TRUTH ITEMS
             intersection = []
@@ -57,21 +56,21 @@ class Metric:
 
             new_count = sorted(count.items(), key=lambda x: x[1], reverse=True)
             pop_ix = [i for i, j in enumerate(new_count) if j[0] == ix][0]
-            if not self.args.include_anns:
+            if not self.include_anns:
                 new_count.pop(pop_ix)
             self.intersection.append(new_count)
 
     def build_ranks(self):
         ranks = {}
-        for sc in self.args.score:
+        for sc in self.score:
             ranks[sc] = []
         return ranks
 
     def calculate_ranks(self, ranks, score_type, gt_ranks=None, modality='i2t'):
         ranks = np.array(ranks)
-        print_str = "{} score with {}".format(score_type.capitalize(), self.args.recall_type.capitalize())
+        print_str = "{} score with {}".format(score_type.capitalize(), self.recall_type.capitalize())
         # TODO: THERE IS A BUG; when IMG_THRESHOLD=1, 'hard', 'recall', 't2i'
-        if score_type == 'hard' and self.args.recall_type == 'recall' and len(ranks.shape)>1:
+        if score_type == 'hard' and self.recall_type == 'recall' and len(ranks.shape) > 1:
             if modality == 'i2t':
                 # This constant is the amount of relevant items
                 num_relevant = self.TEXT_PER_IMG * self.IMG_THRESHOLD
@@ -79,7 +78,7 @@ class Metric:
                 num_relevant = self.IMG_THRESHOLD
 
             for thr in self.RECALL_THRESHOLDS:
-                r_at_thr = sum([sum(r[:thr])/num_relevant for r in ranks])/len(ranks) * 100
+                r_at_thr = sum([sum(r[:thr]) / num_relevant for r in ranks]) / len(ranks) * 100
                 print_str += ", R@{}: {}".format(thr, r_at_thr)
 
         elif score_type == 'hard':
@@ -89,12 +88,13 @@ class Metric:
 
         elif score_type == 'soft':
             for thr in self.RECALL_THRESHOLDS:
-                r_at_thr = sum([sum(r[:thr]) for r in ranks])/len(ranks) * 100
+                r_at_thr = sum([sum(r[:thr]) for r in ranks]) / len(ranks) * 100
                 print_str += ", R@{}: {}".format(thr, r_at_thr)
 
         elif score_type == 'softer':
             for thr in self.RECALL_THRESHOLDS:
-                r_at_thr = 100.0 * ranks[:, :thr].mean(axis=1).mean(axis=0) / (gt_ranks[:, :thr].mean(axis=1).mean(axis=0))
+                r_at_thr = 100.0 * ranks[:, :thr].mean(axis=1).mean(axis=0) / (
+                    gt_ranks[:, :thr].mean(axis=1).mean(axis=0))
                 print_str += ", R@{}: {}".format(thr, r_at_thr)
 
         print(print_str)
@@ -119,7 +119,7 @@ class Metric:
         for ix, sim in enumerate(tqdm.tqdm(self.sims, leave=False)):
             inds = np.argsort(sim)[::-1]
 
-            if not self.args.include_anns:
+            if not self.include_anns:
                 # Remove the index from the similarity
                 gt = list(range(self.TEXT_PER_IMG * ix, self.TEXT_PER_IMG * ix + self.TEXT_PER_IMG, 1))
                 # 100x faster
@@ -140,7 +140,7 @@ class Metric:
 
         for ix, sim in enumerate(tqdm.tqdm(sims, leave=False)):
             inds = np.argsort(sim)[::-1]
-            if not self.args.include_anns:
+            if not self.include_anns:
                 inds = inds[~np.isin(inds, [ix // self.TEXT_PER_IMG])]
                 # inds = np.array([i for i in inds if i != ix // self.TEXT_PER_IMG])
             for sc in args.score:
@@ -187,7 +187,7 @@ class Metric:
         if modality == 'i2t':
             # TODO: Check if correct
             constant = sum(self.metric[relevant_indexes, ix]) + 1e-20
-            rel = [self.metric[i, ix]/constant if i in relevant_indexes else 0 for i in inds[:self.TOP_K]]
+            rel = [self.metric[i, ix] / constant if i in relevant_indexes else 0 for i in inds[:self.TOP_K]]
             # rel = [self.metric[i, ix] if i in relevant_indexes else 0 for i in inds[:10]]
             ranks.append(rel)
 
@@ -203,7 +203,7 @@ class Metric:
             # For normalization
             gt = list(range(self.TEXT_PER_IMG * ix, self.TEXT_PER_IMG * ix + self.TEXT_PER_IMG, 1))
             inds_metric = np.argsort(self.metric[:, ix])[::-1]
-            if not self.args.include_anns:
+            if not self.include_anns:
                 inds_metric = inds_metric[~np.isin(inds_metric, gt)]
                 # inds_metric = np.array([i for i in inds_metric if i not in gt])
             gt_ranks[ix, :] = self.metric[inds_metric[:self.TOP_K]][:, ix]
@@ -212,7 +212,7 @@ class Metric:
             ranks.append(self.metric[:, inds[:self.TOP_K]][ix, :])
             # For normalization
             inds_metric = np.argsort(self.metric[ix, :])[::-1]
-            if not self.args.include_anns:
+            if not self.include_anns:
                 inds_metric = inds_metric[~np.isin(inds_metric, [ix // self.TEXT_PER_IMG])]
                 # inds_metric = np.array([i for i in inds_metric if i !=ix//self.TEXT_PER_IMG])
             gt_ranks[ix, :] = self.metric[:, inds_metric[:self.TOP_K]][ix, :]
@@ -221,7 +221,7 @@ class Metric:
         print("\nModel name:{},\n"
               "Dataset: {},\n"
               "Recall Type: {},\n"
-              "Metric:{},\n".format(self.args.model_name, self.args.dataset, self.args.recall_type, self.args.metric))
+              "Metric:{},\n".format(self.model_name, self.dataset, self.recall_type, self.metric_name))
         print("####I2T#####")
         self.i2t()
         print("####T2I#####")
@@ -236,7 +236,7 @@ if __name__ == "__main__":
 
     parser.add_argument('--dataset', type=str, default='f30k', help='which dataset to use, options are: coco, f30k')
 
-    parser.add_argument('--metric', type=str, default='spice',
+    parser.add_argument('--metric_name', type=str, default='spice',
                         help='which image captioning metric to use, options are: cider, spice')
 
     parser.add_argument('--recall_type', type=str, default='vse_recall', help='Options are recall and vse_recall')
@@ -250,12 +250,33 @@ if __name__ == "__main__":
     parser.add_argument('--threshold', type=int, default=1,
                         help='Threshold of number of relevant samples to compute metrics, options are: 1,2,3')
     parser.add_argument('--recall_thresholds', default=[1, 5, 10, 20, 30], nargs="+", help='K values in Recall_at_K')
-    parser.add_argument('--include_anns', type=bool, default=False,
+    parser.add_argument('--include_anns', type=bool, default=True,
                         help='Include human annotations to define relevant items, options are: True, False')
 
-
     args = parser.parse_args()
-    M = Metric(args)
+
+    if args.dataset == 'coco':
+        with open(os.path.join(args.dataset_path, args.dataset + '_test.json')) as fp:
+            gt = json.load(fp)
+    else:
+        with open(os.path.join(args.dataset_path, args.dataset + '_dataset.json')) as fp:
+            gt = json.load(fp)
+
+    if args.metric_name == 'spice':
+        metric = pd.read_csv(os.path.join(args.metric_path, args.dataset + '_' + args.metric_name + '.csv'), sep=',',
+                             header=None)
+        metric = metric.to_numpy()
+        if args.dataset == 'coco': metric = metric[:, :5000]
+        if args.dataset == 'f30k': metric = metric[:, :1000]
+
+    elif args.metric_name == 'cider':
+        metric = np.load(os.path.join(args.metric_path, args.dataset + '_cider.npy'))
+
+    filename = os.path.join(args.metric_path, 'sims_' + args.model_name + '_' + args.dataset + '_precomp.json')
+    sims = json.load(open(filename, 'r'))
+
+    M = Metric(metric, gt, sims, recall_type=args.recall_type, score=args.score, metric_name=args.metric_name,
+               recall_thresholds=args.recall_thresholds, threshold=args.threshold, dataset=args.dataset,
+               include_anns=args.include_anns, model_name=args.model_name)
     print("\n ... LOADING DATA ...\n")
     M.compute_metrics()
-
