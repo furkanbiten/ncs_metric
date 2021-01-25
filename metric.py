9f4ddd26b78f7f70ecd33a937d7c983a689d6bd4
furkanbiten <furkanbiten@gmail.com>
2021-01-25 15:11:16 +0100

    seperated the class with evaluation

--- metric.py
+++ metric.py
@@ -59,7 +59,7 @@ class Metric:
                 new_count.pop(pop_ix)
             self.intersection.append(new_count)
 
-    def build_ranks(self , sims):
+    def build_ranks(self, sims):
         ranks = {}
         for sc in self.score:
             if sc == 'softer':
@@ -121,7 +121,7 @@ class Metric:
         return relevant_indexes
 
     def i2t(self):
-        ranks = self.build_ranks(sims)
+        ranks = self.build_ranks(self.sims)
         gt_ranks = np.zeros((len(self.sims), self.TOP_K))
 
         for ix, sim in enumerate(tqdm.tqdm(self.sims, leave=False)):
@@ -247,56 +247,3 @@ class Metric:
         scores_t2i = self.t2i()
 
         return {'i2t': scores_i2t, 't2i': scores_t2i}
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--dataset_path', type=str, default='./data', help='ground truth data path')
-
-    parser.add_argument('--metric_path', type=str, default='./out', help='the path that has metrics and model output')
-
-    parser.add_argument('--dataset', type=str, default='coco', help='which dataset to use, options are: coco, f30k')
-
-    parser.add_argument('--metric_name', type=str, default='cider',
-                        help='which image captioning metric to use, options are: cider, spice')
-
-    parser.add_argument('--recall_type', type=str, default='vse_recall', help='Options are recall and vse_recall')
-
-    parser.add_argument('--score', default=['softer'], nargs="+",
-                        help='which scoring method to use, options are: hard, soft, softer')
-
-    parser.add_argument('--model_name', type=str, default='VSRN',
-                        help='which model to use, options are: VSEPP, SCAN, VSRN, CVSE')
-
-    parser.add_argument('--threshold', type=int, default=1,
-                        help='Threshold of number of relevant samples to compute metrics, options are: 1,2,3')
-    parser.add_argument('--recall_thresholds', default=[1, 5, 10, 20, 30], nargs="+", help='K values in Recall_at_K')
-    parser.add_argument('--include_anns', type=bool, default=False,
-                        help='Include human annotations to define relevant items, options are: True, False')
-
-    args = parser.parse_args()
-
-    # if args.dataset == 'coco':
-    #     with open(os.path.join(args.dataset_path, args.dataset + '_test.json')) as fp:
-    #         gt = json.load(fp)
-    # else:
-    #     with open(os.path.join(args.dataset_path, args.dataset + '_dataset.json')) as fp:
-    #         gt = json.load(fp)
-
-    if args.metric_name == 'spice':
-        metric = pd.read_csv(os.path.join(args.metric_path, args.dataset + '_' + args.metric_name + '.csv'), sep=',',
-                             header=None)
-        metric = metric.to_numpy()
-        if args.dataset == 'coco': metric = metric[:, :5000]
-        if args.dataset == 'f30k': metric = metric[:, :1000]
-
-    elif args.metric_name == 'cider':
-        metric = np.load(os.path.join(args.metric_path, args.dataset + '_cider.npy'))
-
-    filename = os.path.join(args.metric_path, 'sims_' + args.model_name + '_' + args.dataset + '_precomp.json')
-    sims = json.load(open(filename, 'r'))
-
-    M = Metric(metric, sims, recall_type=args.recall_type, score=args.score, metric_name=args.metric_name,
-               recall_thresholds=args.recall_thresholds, threshold=args.threshold, dataset=args.dataset,
-               include_anns=args.include_anns, model_name=args.model_name)
-    print("\n ... LOADING DATA ...\n")
-    scores = M.compute_metrics()
